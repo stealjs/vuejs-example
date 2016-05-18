7c03e538e0331144acca7b11d11c2a8460e3c6f3
Marshall Thompson <marshall@creativeideal.net>
2016-05-17 20:03:48 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# steal-vuejs-example
+
+1. Clone this repo.
+2. Run `npm install`
+3. Start an `http-server` in the root directory.
+4. Open in your browser.
---
+++ index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import './styles.less';
+
+new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Welcome to your Vue.js app!',
+    docsURL: 'http://vuejs.org/guide/',
+    gitterURL: 'https://gitter.im/vuejs/vue',
+    forumURL: 'http://forum.vuejs.org/'
+  },
+  methods: {
+    humanizeURL: function (url) {
+      return url
+        .replace(/^https?:\/\//, '')
+        .replace(/\/$/, '');
+    }
+  }
+});
